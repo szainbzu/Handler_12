c92c4915ed077fcee65ded5b4c5560d1106128fb
szainbzu <szain@birzeit.edu>
2023-04-12 15:51:45 +0200

    version1

---
+++ app/src/main/java/edu/cs/birzeit/handler_12/MainActivity.java
@@ -0,0 +1,52 @@
+package edu.cs.birzeit.handler_12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn, btn2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        btn2 = findViewById(R.id.btn2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HandlerThread handlerThread = new HandlerThread("Background task");
+                handlerThread.start();
+                Handler handler = new Handler(handlerThread.getLooper());
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        int x=0, y=0;
+                        for(int j = 1; j<100000; j++){
+                            for(x = 1; x<100000; x++){
+                                y = x;
+                            }
+                        }
+                        Toast.makeText(MainActivity.this, "Work done "+y,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
